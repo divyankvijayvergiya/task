c6a0d5a8057e2b3b827f8a2db7b8f6b62a7528eb
Divyank Vijayvergiya <divyankvijay.96@gmail.com>
2018-09-01 13:38:03 +0530

    commit

--- app/src/main/java/io/funswitch/task/MainActivity.java
+++ app/src/main/java/io/funswitch/task/MainActivity.java
@@ -4,10 +4,14 @@ import android.animation.ValueAnimator;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView1, textView2, textView3, textView4, textView5, textView6;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         textView4 = findViewById(R.id.tt3);
         textView5 = findViewById(R.id.tt4);
         textView6 = findViewById(R.id.tt5);
+        linearLayout = findViewById(R.id.ll);
 
         ValueAnimator animator = ValueAnimator.ofInt(1, 9);
         animator.setDuration(1000);
@@ -121,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 // This method will be executed once the timer is over
-                ValueAnimator animator1 = ValueAnimator.ofInt(60, 12);
+                ValueAnimator animator1 = ValueAnimator.ofInt(90, 20);
                 animator1.setDuration(4000);
                 animator1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                     public void onAnimationUpdate(ValueAnimator animation) {
@@ -132,6 +137,8 @@ public class MainActivity extends AppCompatActivity {
                         textView4.setTextSize((int) animation.getAnimatedValue());
                         textView5.setTextSize((int) animation.getAnimatedValue());
                         textView6.setTextSize((int) animation.getAnimatedValue());
+
+                        linearLayout.setGravity(Gravity.CENTER);
                     }
                 });
                 animator1.start();
